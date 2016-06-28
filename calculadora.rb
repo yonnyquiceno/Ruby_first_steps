83ab6823d60ded72f5738aa45053d83acc4cde6a
yonnyquiceno <yonnyquiceno@gmail.com>
2016-06-27 20:45:30 -0500

    clases operaciones aritméticas simples

---
+++ calculadora.rb
@@ -0,0 +1,21 @@
+class Calculadora
+  def self.suma(a, b)
+    a + b
+  end
+
+  def self.resta(a, b)
+    a - b
+  end
+
+  def self.multiplicacion(a, b)
+    a * b
+  end
+
+  def self.division(a, b)
+    a / b.to_f
+  end
+
+  def self.concat(a, b)
+    a + b
+  end
+end
---
+++ initial_class.rb
@@ -0,0 +1,34 @@
+# Esta es la forma como se definen
+# clases en Ruby
+class InitialClass
+  # Esta es la forma como se declara
+  # un metodo o función en Ruby
+  # por estandar se usa nombre_metodo
+
+  def self.say_hello
+    puts 'Hi There !'
+  end
+
+  def self.what_im_i(element)
+    # Note el uso de comilla doble
+    # para hacer interpolacion
+    puts "#{element} a #{element.class}!"
+    # Note el uso de return para
+    # devolver un valor desde el
+    # metodo
+    element.class
+  end
+
+  # Cuando un metodo devuelve true o
+  # false, por convencion se llama
+  # nombre_metodo?
+  def self.im_i_a_number?(element)
+    # No es necesario usar return
+    # si deseo retornar un valor.
+    element.is_a?(Fixnum)
+  end
+
+  def self.im_i_a_string?(element)
+    element.is_a?(String)
+  end
+end
