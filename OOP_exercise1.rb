0d36a6b748fc93e14c662be0cf6d19424b5d6af9
Your Name <yonnyquiceno@gmail.com>
2016-07-08 19:43:17 -0500

    Modifica método comprar de clase Comprador
    Ahora el metodo comprar no pide una instancia de la clase Producto sino que pide el nombre del producto directamente
    fue necesario añadir una clase keeper para almacenar los nuevos productos a medida que son creados.

--- OOP_exercise1.rb
+++ OOP_exercise1.rb
@@ -14,50 +14,82 @@ class Persona
 end
 
 class Vendedor < Persona
-  attr_accessor :ganado
+  attr_accessor :ganado, :productos
 
   def initialize(nombres, apellidos, cedula, telefono)
     super(nombres, apellidos, cedula, telefono)
     @ganado = 0
+    @productos=[]
   end
 end
 
 class Comprador < Persona
-  attr_accessor :comprados, :tope_de_dinero, :gastado
+  attr_accessor :comprados, :tope_de_dinero, :gastado, :vendedor
 
-  def initialize(nombres, apellidos, cedula, telefono, tope_de_dinero, vendedor)
+  def initialize(nombres, apellidos, cedula, telefono, tope_de_dinero)
     super(nombres, apellidos, cedula, telefono)
 
     @tope_de_dinero = tope_de_dinero
     @vendedor = vendedor
     @gastado = 0
+
+
+  end
+
+  def buscar_producto(nombre_producto)
+    productos = Keeper.get_productos
+
+    for i in 0..productos.size
+      if productos[i].include?(nombre_producto)
+        indice_producto=i
+        break
+      end
+    end
+
+    productos[indice_producto] # select selecciona los elementos que cumplen la condicion y los retorna en un array
+
   end
 
   def comprar(producto, cantidad)
-    producto.stock = producto.stock - cantidad
-    @gastado += (producto.precio_venta * cantidad)
-    print 'no tiene mas dinero' if @gastado > @tope_de_dinero    
-    @vendedor.ganado = @vendedor.ganado + (producto.precio_venta * cantidad) - (producto.precio_base * cantidad)
-    @vendedor.lista_productos << "#{cantidad} #{producto.nombre}(s)"
-    @lista_productos.push("#{cantidad} #{producto.nombre}(s)")
+    producto = buscar_producto(producto)
+    producto[1] = producto[1] - cantidad
+    @gastado += (producto[3] * cantidad)
+    print 'no tiene mas dinero' if @gastado > @tope_de_dinero
+    @vendedor.ganado = @vendedor.ganado + (producto[3] * cantidad) - (producto[2]* cantidad)
+    @vendedor.lista_productos << "#{cantidad} #{producto[0]}(s)"
+    @lista_productos.push("#{cantidad} #{producto[0]}(s)")
   end
+
 end
 
 class Producto
   attr_accessor :nombre, :stock, :precio_base, :precio_venta
   def initialize(nombre, stock, precio_base, precio_venta)
     @nombre = nombre
-    @stock = stock
+    @stockini = stock
     @precio_base = precio_base
     @precio_venta = precio_venta
+    Keeper.set_productos([@nombre, @stockini, @precio_base, @precio_venta])
   end
+  def self.get_productos
+    @@productos
+  end
+
+  def stock
+    return Keeper.get_productos
+  end
+
 end
 
-class Loader
-  def self.load
-    @@v1 = Vendedor.new('Roberto', 'Meneses', 1234, 111_222_333)
-    @@c1 = Comprador.new('Armando', 'Barrera', 1111, 2222, 500_000, v1)
-    @@p1 = Producto.new('silla', 40, 40_000, 60_000)
+class Keeper
+  def initialize()
+    @@productos=[]
+  end
+  def self.set_productos(producto)
+    @@productos << producto
+  end
+  def self.get_productos
+    @@productos
   end
 end
 
